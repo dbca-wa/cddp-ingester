fb2e514374c42ef0b38f4ac8c113c351aa73fda8
Ashley Felton <ashley@ropable.com>
2019-12-05 15:41:32 +0800

    Refactor ingester to use multiprocessing.

--- ingester.py
+++ ingester.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import logging
+import multiprocessing
 import os
 import subprocess
 import sys
@@ -14,27 +15,21 @@ formatter = logging.Formatter('%(asctime)s | %(message)s')
 handler.setFormatter(formatter)
 LOGGER.addHandler(handler)
 
+# Development environment: define variables in .env
+dot_env = os.path.join(os.getcwd(), '.env')
+if os.path.exists(dot_env):
+    load_dotenv()
 
-def ingest_cddp(cddp_path=None):
+
+def parse_cddp(cddp_path=None):
     '''This function expects the CDDP filepath to be passed in
     (e.g. /mnt/GIS-CALM/GIS1-Corporate/Data/GDB), in order to walk the path and locate
     file geodatabases for copying to the database.
+    Returns a list of tuples containing (path, layer_name) pairs.
     '''
-    # Development environment: define variables in .env
-    dot_env = os.path.join(os.getcwd(), '.env')
-    if os.path.exists(dot_env):
-        load_dotenv()
-
     if not cddp_path:
         # Assume that this path set via an environment variable if not explicitly passed in.
         cddp_path = os.getenv('CDDP_PATH')
-    pg_string = 'host={} user={} password={} dbname={}'.format(
-        os.getenv('DATABASE_HOST'),
-        os.getenv('DATABASE_USERNAME'),
-        os.getenv('DATABASE_PASSWORD'),
-        os.getenv('DATABASE_NAME'),
-    )
-    ogr2ogr_cmd = 'ogr2ogr -overwrite -f PostgreSQL PG:"{pg_string}" {file_gdb} {layer_name}'
 
     gdb_paths = []
     for i in os.walk(cddp_path):
@@ -43,9 +38,9 @@ def ingest_cddp(cddp_path=None):
         if i[0].endswith('.gdb'):
             gdb_paths.append(i[0])
 
-    for file_gdb in gdb_paths:
-        LOGGER.info(file_gdb)
+    datasets = []
 
+    for file_gdb in gdb_paths:
         try:
             gdb_layers = subprocess.check_output('ogrinfo -ro -so -q {}'.format(file_gdb), shell=True)
         except subprocess.CalledProcessError as e:
@@ -57,42 +52,70 @@ def ingest_cddp(cddp_path=None):
 
         for layer in layers:
             layer_name = layer.split()[1].decode()
-            LOGGER.info(layer_name)
+            datasets.append((file_gdb, layer_name))
 
-            try:
-                cmd = ogr2ogr_cmd.format(pg_string=pg_string, file_gdb=file_gdb, layer_name=layer_name)
-                result = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
-            except subprocess.CalledProcessError as e:
-                LOGGER.exception('ERROR: ogr2ogr step failed for layer {} in {}'.format(layer, file_gdb))
-                LOGGER.info(e.cmd)
-                continue
+    return datasets
 
-            # NONSTANDARD GEOMETRY TYPE HANDLING
-            # The ogr2ogr copy operation might fail due to the dataset geometry not matching one of
-            # the standard ones that the command expects (e.g. Multi Surface), but the command still
-            # returns 0.
-            # Check the stdout content for indicators that the copy failed.
-            if b'COPY statement failed' in result and b'type Multi Surface' in result:
-                LOGGER.warning('Copy statement failed, geometry type Multi Surface, trying explicit geom type')
-                # Manually set the geometry type to MULTIPOLYGON:
-                cmd = 'ogr2ogr -overwrite -f -nlt MULTIPOLYGON PostgreSQL PG:"{}" {} {}'.format(
-                    pg_string, file_gdb, layer_name)
-                try:
-                    subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
-                except subprocess.CalledProcessError as e:
-                    LOGGER.exception('ERROR: ogr2ogr step failed for layer {} in {}'.format(layer, file_gdb))
-                    LOGGER.info(e.cmd)
-            elif b'COPY statement failed' in result and b'type Multi Curve' in result:
-                LOGGER.warning('Copy statement failed, geometry type Multi Curve, trying explicit geom type')
-                # Manually set the geometry type to MULTILINESTRING:
-                cmd = 'ogr2ogr -overwrite -f -nlt MULTILINESTRING PostgreSQL PG:"{}" {} {}'.format(
-                    pg_string, file_gdb, layer_name)
-                try:
-                    subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
-                except subprocess.CalledProcessError as e:
-                    LOGGER.exception('ERROR: ogr2ogr step failed for layer {} in {}'.format(layer, file_gdb))
-                    LOGGER.info(e.cmd)
+
+def ingest_layer(data):
+    """This function expects to be passed a tuple containing (path, layer_name) pairs for
+    import to a PostgreSQL database using ogr2ogr.
+    """
+    file_gdb, layer_name = data[0], data[1]
+    pg_string = 'host={} user={} password={} dbname={}'.format(
+        os.getenv('DATABASE_HOST'),
+        os.getenv('DATABASE_USERNAME'),
+        os.getenv('DATABASE_PASSWORD'),
+        os.getenv('DATABASE_NAME'),
+    )
+    ogr2ogr_cmd = 'ogr2ogr -overwrite -f PostgreSQL PG:"{pg_string}" {file_gdb} {layer_name}'
+    LOGGER.info('Copying layer {}'.format(layer_name))
+
+    try:
+        cmd = ogr2ogr_cmd.format(pg_string=pg_string, file_gdb=file_gdb, layer_name=layer_name)
+        result = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        LOGGER.exception('ERROR: ogr2ogr step failed for layer {} in {}'.format(layer_name, file_gdb))
+        LOGGER.info(e.cmd)
+        return
+
+    # NONSTANDARD GEOMETRY TYPE HANDLING
+    # The ogr2ogr copy operation might fail due to the dataset geometry not matching one of
+    # the standard ones that the command expects (e.g. Multi Surface), but the command still
+    # returns 0.
+    # Check the stdout content for indicators that the copy failed.
+    if b'COPY statement failed' in result and b'type Multi Surface' in result:
+        LOGGER.warning('Copy statement failed, geometry type Multi Surface, trying explicit geom type')
+        # Manually set the geometry type to MULTIPOLYGON:
+        cmd = 'ogr2ogr -overwrite -f -nlt MULTIPOLYGON PostgreSQL PG:"{}" {} {}'.format(
+            pg_string, file_gdb, layer_name)
+        try:
+            subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as e:
+            LOGGER.exception('ERROR: ogr2ogr step failed for layer {} in {}'.format(layer_name, file_gdb))
+            LOGGER.info(e.cmd)
+            return
+    elif b'COPY statement failed' in result and b'type Multi Curve' in result:
+        LOGGER.warning('Copy statement failed, geometry type Multi Curve, trying explicit geom type')
+        # Manually set the geometry type to MULTILINESTRING:
+        cmd = 'ogr2ogr -overwrite -f -nlt MULTILINESTRING PostgreSQL PG:"{}" {} {}'.format(
+            pg_string, file_gdb, layer_name)
+        try:
+            subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as e:
+            LOGGER.exception('ERROR: ogr2ogr step failed for layer {} in {}'.format(layer_name, file_gdb))
+            LOGGER.info(e.cmd)
+            return
+
+    LOGGER.info('Layer {} completed'.format(layer_name))
+
+
+def mp_handler(data):
+    # Use a multiprocessing Pool to ingest datasets in parallel.
+    p = multiprocessing.Pool(4)
+    p.map(ingest_layer, data)
 
 
 if __name__ == "__main__":
-    ingest_cddp()
+    data = parse_cddp()
+    mp_handler(data)
